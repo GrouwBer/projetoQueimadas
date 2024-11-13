7b34574e8c2ba4d4ceb2f0b1f908155302160c95
GrouwBer <91547544+GrouwBer@users.noreply.github.com>
2024-11-12 21:23:45 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import HeatmapLayer from './HeatmapLayer'; // Import the custom HeatmapLayer
+import Draggable from 'react-draggable';
+import { ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css';
 import './App.css';
-
-interface HeatmapPoint {
-  lat: number;
-  lng: number;
-  intensity: number;
-}
+import Grafico from './grafico'; // Certifique-se de que o caminho e a extensão estão corretos
 
 function App() {
-  const heatmapData: HeatmapPoint[] = [
-    { lat: -21.7883, lng: -46.5626, intensity: 1 },
+  const [verGrafico, MostraGrafico] = useState(false);
+
+  const heatmapData = [
+    { lat: -21.837799, lng: -46.558914, intensity: 1 },
     { lat: -21.7883, lng: -46.5625, intensity: 1 },
     // more data points
   ];
 
   return (
-    <MapContainer center={[-21.7883, -46.5625]} zoom={13} scrollWheelZoom={true}>
-      <HeatmapLayer points={heatmapData} />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="container" style={{ position: 'relative', height: '100vh' }}>
+      <div className="map-container" style={{ height: '100%' }}>
+        <MapContainer center={[-21.7883, -46.5625]} zoom={13} scrollWheelZoom={true} className="leaflet-container">
+          <HeatmapLayer points={heatmapData} />
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      <button onClick={() => MostraGrafico(!verGrafico)} style={{ position: 'fixed', top: '10px', right: '10px', zIndex: 1000 }}>
+        {verGrafico ? 'Fechar Gráfico' : 'Gráfico'}
+      </button>
+      {verGrafico && (
+        <div  style={{ zIndex: 1000, position: 'absolute', top: 0, left: 0 }}>
+          <Draggable>
+            <div>
+            <ResizableBox width={300} height={150} minConstraints={[200, 200]} maxConstraints={[600, 600]}>
+             
+                
+                  <Grafico />
+              
+            </ResizableBox>
+            </div>
+          </Draggable>
+        </div>
+      )}
+    </div>
   );
+
 }
 
 export default App;
---
+++ src/grafico.tsx
@@ -0,0 +1,73 @@
+import { useRef, useEffect } from 'react';
+import { Chart, registerables } from 'chart.js';
+import { labels, ruindade, qualidade } from './dadosGrafico';
+// Registre todos os componentes necessários
+Chart.register(...registerables);
+
+const Grafico = () => {
+  const chartRef = useRef<HTMLCanvasElement>(null);
+  const chartInstanceRef = useRef<Chart | null>(null);
+
+  useEffect(() => {
+    console.log('Grafico component mounted');
+    if (chartRef.current) {
+      console.log('Chart ref is set', chartRef.current);
+
+      // Destrua o gráfico existente antes de criar um novo
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+
+      chartInstanceRef.current = new Chart(chartRef.current, {
+        type: 'bar', // Tipo de gráfico, pode ser 'line', 'bar', 'pie', etc.
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Qualida do Ar',
+              data: qualidade,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+              ],
+              borderWidth: 1,
+            },
+            {
+                label: 'Ruindade do AR',
+                data: ruindade,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+              },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+
+      console.log('Chart instance created', chartInstanceRef.current);
+    }
+
+    // Cleanup function to destroy the chart when the component unmounts
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div className='grafico'>
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
+};
+
+export default Grafico;
